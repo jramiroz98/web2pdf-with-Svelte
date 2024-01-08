bd978ca9556df2e43a5395560d7e9aeb0d832d49
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2024-01-08 14:20:31 -0600

    prints successfully

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { jsPDF } from 'jspdf';
+	function printPDF() {
+		const doc = new jsPDF();
+		doc.html(document.getElementById('toPrint'), {
+			callback: function (doc) {
+				doc.save();
+			},
+			x: 50,
+			y: 50
+		});
+	}
+</script>
+
+<div id="toPrint">
+	<h1>Hello PDF</h1>
+	<p style="color:red">Hola mundo</p>
+</div>
+<a href="https://raw.githack.com/MrRio/jsPDF/master/docs/module-html.html">documentation</a>
+
+<button on:click={() => printPDF()}>Generate PDF</button>
